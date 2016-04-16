a051ee2bb49d7ba9f6ce91cdb10d234a6aad1770
Nickc92 <nickc92>
2016-04-15 20:59:53 -0400

    blah

--- procMusicXML.py
+++ procMusicXML.py
@@ -1,4 +1,4 @@
-import tachyonxml
+import tachyonxml, commands, re
 import xml.dom
 import numpy as np, sys
 
@@ -38,70 +38,89 @@ for iFifth in range(5):
     for note in scale:
         noteToOffsets[note[0]] = noteOffsets[note]
     flatScaleOffsets.append(noteToOffsets)
-    print scale, noteToOffsets
+    #print scale, noteToOffsets
 
+def procFile(fname):
+    musicxml = tachyonxml.XMLParseFile(open(fname))
+    score = musicxml.getChildNode('score-partwise')
+    divisions = None
+    fifths = 0
+    noteOffsets = None
+    totalScore = []
+    scaleOffset = 0
+    for part in score.getChildNodes('part'):
+        for iMeasure, measure in enumerate(part.getChildNodes('measure')):
+            attr = measure.getChildNode('attributes')
+            if attr != None:
+                key = attr.getChildNode('key')
+                if key != None:
+                    f = key.getChildNode('fifths')
+                    if f != None:
+                        fifths = int(f.getValue())
+                        if fifths > 0: noteOffsets = sharpScaleOffsets[fifths]
+                        else: noteOffsets = flatScaleOffsets[fifths]
+                        scaleOffset = (fifths * 7) % 12
+                divisions = int(attr.getChildNode('divisions').getValue())
+                beats = int(attr.getChildNode('beats').getValue())
+                beatType = int(attr.getChildNode('beat-type').getValue())
 
-fname = 'lg-73176162.xml'
-if len(sys.argv) > 1: fname = sys.argv[1]
+            totQuarterNotes = 4 * beats / beatType
+            measureTotDivs = int(totQuarterNotes * divisions)
+            #print totQuarterNotes, beats, beatType, divisions, measureTotDivs
+            measureDat = [[] for i in range(measureTotDivs)]
+            pos = 0
+            lastLen = None
+            for node in measure.theElem.childNodes:
+                if node.nodeType == node.ELEMENT_NODE:
+                    xmlNode = tachyonxml.XMLNode(node)
+                    if xmlNode.name == 'backup':
+                        duration = int(xmlNode.getChildNode('duration').getValue())
+                        pos -= duration
+                    if xmlNode.name == 'note':
+                        if xmlNode.getChildNode('duration') == None: continue
+                        duration = int(xmlNode.getChildNode('duration').getValue())
+                        chord = xmlNode.getChildNode('chord')
+                        if chord != None:
+                            if lastLen == None: 
+                                pass
+                                #print 'chord with no lastLen!!!'
+                            else: pos -= lastLen
+                        lastLen = duration
+                        rest = xmlNode.getChildNode('rest')
+                        if rest != None:
+                            pass
+                            #print 'rest'
+                        pitch = xmlNode.getChildNode('pitch')
+                        if pitch != None:
+                            note = pitch.getChildNode('step').getValue()
+                            oct = pitch.getChildNode('octave')
+                            if oct != None: octave = int(oct.getValue())
+                            alter = 0
+                            alt = pitch.getChildNode('alter')
+                            if alt != None: alter = int(alt.getValue())
+                            noteNumber = octave * 12 + noteOffsets[note] + alter - scaleOffset
+                            measureDat[pos].append([noteNumber, duration])
+                            #print 'measure: %d, pos: %d, pitch: %d, duration: %d'%(iMeasure, pos, noteNumber, duration)
+                        pos += duration
+            #print 'position at end of measure:', pos
+            totalScore.extend(measureDat)
+    return totalScore
 
-musicxml = tachyonxml.XMLParseFile(open(fname))
-score = musicxml.getChildNode('score-partwise')
-divisions = None
-fifths = 0
-noteOffsets = None
-for part in score.getChildNodes('part'):
-    for iMeasure, measure in enumerate(part.getChildNodes('measure')):
-        attr = measure.getChildNode('attributes')
-        if attr != None:
-            key = attr.getChildNode('key')
-            if key != None:
-                f = key.getChildNode('fifths')
-                if f != None:
-                    fifths = int(f.getValue())
-                    if fifths > 0: noteOffsets = sharpScaleOffsets[fifths]
-                    else: noteOffsets = flatScaleOffsets[fifths]
-            divisions = int(attr.getChildNode('divisions').getValue())
-            beats = int(attr.getChildNode('beats').getValue())
-            beatType = int(attr.getChildNode('beat-type').getValue())
-
-        totQuarterNotes = 4 * beats / beatType
-        measureTotDivs = int(totQuarterNotes * divisions)
-        #print totQuarterNotes, beats, beatType, divisions, measureTotDivs
-        measureDat = [[] for i in range(measureTotDivs)]
-        pos = 0
-        lastLen = None
-        for node in measure.theElem.childNodes:
-            if node.nodeType == node.ELEMENT_NODE:
-                xmlNode = tachyonxml.XMLNode(node)
-                if xmlNode.name == 'backup':
-                    duration = int(xmlNode.getChildNode('duration').getValue())
-                    pos -= duration
-                if xmlNode.name == 'note':
-                    if xmlNode.getChildNode('duration') == None: continue
-                    duration = int(xmlNode.getChildNode('duration').getValue())
-                    chord = xmlNode.getChildNode('chord')
-                    if chord != None:
-                        if lastLen == None: print 'chord with no lastLen!!!'
-                        else: pos -= lastLen
-                    lastLen = duration
-                    rest = xmlNode.getChildNode('rest')
-                    if rest != None:
-                        pass
-                        #print 'rest'
-                    pitch = xmlNode.getChildNode('pitch')
-                    if pitch != None:
-                        note = pitch.getChildNode('step').getValue()
-                        oct = pitch.getChildNode('octave')
-                        if oct != None: octave = int(oct.getValue())
-                        alter = 0
-                        alt = pitch.getChildNode('alter')
-                        if alt != None: alter = int(alt.getValue())
-                        noteNumber = octave * 12 + noteOffsets[note] + alter
-                        measureDat[pos].append([noteNumber, duration])
-                        print 'measure: %d, pos: %d, pitch: %d, duration: %d'%(iMeasure, pos, noteNumber, duration)
-                    pos += duration
-        print 'position at end of measure:', pos
-        print measureDat
+#fname = 'lg-73176162.xml'
+#if len(sys.argv) > 1: fname = sys.argv[1]
+#totScore = procFile(fname)
+#print totScore
+#fls = open('Music/clavierFiles.txt').read().splitlines()
+#fls = ['Music/' + re.sub(':', '', fl) for fl in fls]
+fls = commands.getoutput('ls Music/*xml').splitlines()
+ofl = open('allnotes.txt', 'w')
+for f in fls:
+    print f
+    try:
+        result = procFile(f)
+        if result != None: 
+            print >>ofl, result
+    except: pass
                     
                 
                     
